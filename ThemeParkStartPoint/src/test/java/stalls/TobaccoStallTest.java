6fb9471742290b53ad173bc6fe7864a2e0e956e6
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-15 16:42:01 +0000

    tobacco security tested

--- ThemeParkStartPoint/src/test/java/stalls/TobaccoStallTest.java
+++ ThemeParkStartPoint/src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,22 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitor1 = new Visitor(21, 182, 40.00);
+        visitor2 = new Visitor(17, 180, 40.00);
+
     }
 
     @Test
@@ -27,5 +33,15 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void canVisit(){
+        assertTrue(tobaccoStall.isAllowedTo(visitor1));
+    }
+
+    @Test
+    public void cannotVisit(){
+        assertFalse(tobaccoStall.isAllowedTo(visitor2));
     }
 }
