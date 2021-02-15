6d4db21b6ea7ec016403a0a406c58e3783cda54c
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-15 17:00:32 +0000

    dogems ticket tests

--- ThemeParkStartPoint/src/test/java/attractions/DodgemTest.java
+++ ThemeParkStartPoint/src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,21 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        visitor1 = new Visitor(9, 99, 5.00);
+        visitor2 = new Visitor(19, 179, 45.00);
     }
 
 
@@ -28,6 +33,16 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void youngPersonPrice(){
+        assertEquals(2.25, dodgems.priceFor(visitor1), 0.01);
+    }
+
+    @Test
+    public void olderPersonPrice(){
+        assertEquals(4.50, dodgems.priceFor(visitor2), 0.01);
     }
 
 
