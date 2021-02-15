89e52f8740b2bbf8d21bff1ce389060404337bd6
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-15 16:37:27 +0000

    12yr old can visit playground

--- ThemeParkStartPoint/src/test/java/attractions/PlaygroundTest.java
+++ ThemeParkStartPoint/src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,22 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PlaygroundTest {
     Playground playground;
-
+    Visitor visitor1;
+    Visitor visitor2;
+    Visitor visitor3;
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitor1 = new Visitor(12, 123, 10.00);
+        visitor2 = new Visitor(15, 123, 10.00);
+        visitor3 = new Visitor(16, 123, 10.00);
     }
 
     @Test
@@ -27,4 +34,10 @@ public class PlaygroundTest {
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
     }
+
+    @Test
+    public void canVisit(){
+        assertTrue(playground.isAllowedTo(visitor1));
+    }
+
 }
